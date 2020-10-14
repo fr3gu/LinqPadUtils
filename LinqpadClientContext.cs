2fcdbb37a0b81b8b8a5a235f957ff25c9505679f
Fr3gU <fr3gu77@gmail.com>
2020-10-14 11:48:20 +0200

    Uppdaterat Hilda.Shared.Contracts.* och fixat implementation i LinqpadClientContext

--- LinqpadClientContext.cs
+++ LinqpadClientContext.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Net;
+using System.Security.Principal;
 using Hilda.Shared.Contracts.BLL.Infrastructure;
 
 namespace LinqPadUtils
@@ -7,13 +9,15 @@ namespace LinqPadUtils
     {
         public string UserName { get; set; }
         public string SessionId { get; set; }
-        public ICustomPrincipal SecurityPrincipal { get; set; }
+        public IPrincipal SecurityPrincipal { get; }
+        public IPAddress IpAddress { get; }
 
         public LinqPadClientContext()
         {
             UserName = "LinqPad query";
             SessionId = Guid.NewGuid().ToString();
-            SecurityPrincipal = null;
+            SecurityPrincipal = new GenericPrincipal(new GenericIdentity("LinqPadUser"), null);
+            IpAddress = IPAddress.Parse("127.0.0.1");
         }
     }
 }
